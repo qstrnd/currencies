2cd5a08c0fa19a6bca6770e7ae10a28ca253155c
Andrey Yakovlev <andrey@yakovlev.tech>
2022-10-08 11:49:30 +0400

    Add Network package

---
+++ Packages/Network/Sources/Network/NetworkError.swift
@@ -0,0 +1,25 @@
+//
+//  NetworkError.swift
+//  
+//
+//  Created by Andrey Yakovlev on 08.10.2022.
+//
+
+import Foundation
+
+public enum NetworkError: Error {
+    case invalidURL
+
+    public enum InvalidResponse: Error {
+        /// For 404
+        case notFound
+
+        /// For 400s other than 404
+        case clientError
+
+        /// For 500s
+        case serverError
+
+        case other
+    }
+}
