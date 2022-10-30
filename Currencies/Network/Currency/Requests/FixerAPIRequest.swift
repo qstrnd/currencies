0f6b19bdc45adce2474cd226767ec6217f83ef5a
Andrey Yakovlev <andrey@yakovlev.tech>
2022-10-30 12:39:57 +0400

    Refactor FixerAPI request

--- Currencies/Network/Currency/Requests/FixerAPIRequest.swift
+++ Currencies/Network/Currency/Requests/FixerAPIRequest.swift
@@ -7,10 +7,15 @@
 
 import Network
 
-protocol FixerAPIRequest: Request {}
+protocol FixerAPIRequest: Request {
+    var fixerPath: String { get }
+}
 
 extension FixerAPIRequest {
     var host: String { "api.apilayer.com" }
     var requestType: RequestType { .get }
     var addAuthorizationToken: Bool { true }
+    var path: String {
+        "fixer/\(fixerPath)"
+    }
 }
--- Currencies/Network/Currency/Requests/Symbols/SymbolsRequest.swift
+++ Currencies/Network/Currency/Requests/Symbols/SymbolsRequest.swift
@@ -8,5 +8,5 @@
 import Network
 
 struct SymbolsRequest: FixerAPIRequest {
-    let path = "fixer/symbols"
+    let fixerPath = "symbols"
 }
